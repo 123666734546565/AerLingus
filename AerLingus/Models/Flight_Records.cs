52030316f447dd914499187f97b8a9073bb342cf
Aleksa <aleksa.aking.1999@gmail.com>
2019-08-16 14:58:48 +0200

    Ne radi Upload za Api i ne radi nvarchar max u bazi

--- AerLingus/Models/Flight_Records.cs
+++ AerLingus/Models/Flight_Records.cs
@@ -49,45 +49,5 @@ namespace AerLingus.Models
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
-
-        public Flight_Records()
-        {
-
-        }
-
-        public Flight_Records(Flight_Records record)
-        {
-            identifierNo = record.identifierNo;
-            transactionType = record.transactionType;
-            otherFFPNo = record.otherFFPNo;
-            otherFFPScheme = record.otherFFPScheme;
-            firstName = record.firstName;
-            lastName = record.lastName;
-            partnerTransactionNo = record.partnerTransactionNo;
-            bookingDate = record.bookingDate;
-            departureDate = record.departureDate;
-            origin = record.origin;
-            destination = record.destination;
-            bookingClass = record.bookingClass;
-            cabinClass = record.cabinClass;
-            marketingFlightNo = record.marketingFlightNo;
-            marketingAirline = record.marketingAirline;
-            operatingFlightNo = record.operatingFlightNo;
-            operatingFlightNo = record.operatingAirline;
-            ticketNo = record.ticketNo;
-            externalPaxID = record.externalPaxID;
-            couponNo = record.couponNo;
-            pnrNo = record.pnrNo;
-            distance = record.distance;
-            baseFare = record.baseFare;
-            discountBase = record.discountBase;
-            exciseTax = record.exciseTax;
-            customerType = record.customerType;
-            promotionCode = record.promotionCode;
-            ticketCurrency = record.ticketCurrency;
-            targetCurrency = record.targetCurrency;
-            exchangeRate = record.exchangeRate;
-            fareBasis = record.fareBasis;
-        }
     }
 }
