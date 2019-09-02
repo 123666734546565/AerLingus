a0151d5e88d466b758c68e82fe550ce6e2f810c8
Milos Spasojevic <milos24217@its.edu.rs>
2019-09-02 12:15:21 +0200

    napravljen searcch model

--- AerLingus/Helpers/SearchFlightRecord.cs
+++ AerLingus/Helpers/SearchFlightRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,40 @@ namespace AerLingus.Helpers
 {
     public class SearchFlightRecord
     {
-    }
+		[Display(Name = "Identifier Number")]
+		public string S_identifierNo { get; set; }
+
+		[Display(Name = "Other FFP Number")]
+		public string S_otherFFPNo { get; set; }
+
+		[Display(Name = "First Name")]
+		public string S_firstName { get; set; }
+
+		[Display(Name = "Last Name")]
+		public string S_lastName { get; set; }
+
+		[Display(Name = "Departure Date")]
+		public DateTime S_departureDate { get; set; }
+
+		[Display(Name = "Origin")]
+		public string S_Origin { get; set; }
+
+		[Display(Name = "Destination")]
+		public string S_destination { get; set; }
+
+		[Display(Name = "Booking Class")]
+		public string S_bookingClass { get; set; }
+
+		[Display(Name = "Operating Airline")]
+		public string S_operatingAirline { get; set; }
+
+		[Display(Name = "Ticket Number")]
+		public string S_ticketNo { get; set; }
+
+		[Display(Name = "External Pax ID")]
+		public string S_externalPaxID { get; set; }
+
+		[Display(Name = "PNR Number")]
+		public string S_pnrNo { get; set; }
+	}
 }
