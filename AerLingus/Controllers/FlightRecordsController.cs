e3a52686a80df93a3f3159042231c9f4b8a28f75
Aleksa <aleksa.aking.1999@gmail.com>
2019-09-02 12:35:42 +0200

    Dodat formular

--- AerLingus/Controllers/FlightRecordsController.cs
+++ AerLingus/Controllers/FlightRecordsController.cs
@@ -61,88 +61,6 @@ namespace AerLingus.Controllers
             }
         }
 
-
-
-        //[System.Web.Http.HttpPost]
-        //public ActionResult DragAndDrop()
-        //{
-        //    int counter = 1;
-        //    string fname = string.Empty;
-
-
-
-        //    try
-        //    { 
-        //        foreach (string fileName in Request.Files)
-        //        {
-
-        //            string errorMessage = "";
-        //            HttpPostedFileBase file = Request.Files[fileName];
-        //            fname = file.FileName;
-        //            if (file != null && file.ContentLength > 0)
-        //            {
-        //                var path = Path.Combine(Server.MapPath("~/UploadedFiles"));
-        //                string pathString = Path.Combine(path.ToString());
-
-        //                if(Path.GetExtension(file.FileName) != ".csv")
-        //                {
-        //                    errorMessage += errorMessage + "File[" + counter + "] - ERROR 421: File must have .csv extension" + "\n";
-        //                    counter++;
-        //                    continue;
-        //                }
-
-
-        //                string fileName1 = fname + Path.GetExtension(file.FileName);
-        //                string uploadPath = string.Format("{0}\\{1}", pathString, fileName1);
-        //                file.SaveAs(uploadPath);
-
-        //                FlightRecordsApiController api = new FlightRecordsApiController()
-        //                {
-        //                    Request = new HttpRequestMessage(),
-        //                    Configuration = new HttpConfiguration()
-        //                };
-
-        //                var returnedStatusCode = api.Upload(file).StatusCode;
-
-        //                if (returnedStatusCode == System.Net.HttpStatusCode.OK)
-        //                   // ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - STATUS 201: Successful" + "\n";
-        //                errorMessage += errorMessage + "File[" + counter + "] - STATUS 201: Successfull" + "\n";
-        //                //else
-        //                //{
-        //                //    if (returnedStatusCode == System.Net.HttpStatusCode.NotFound)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 404: No file selected" + "\n";
-        //                //        errorMessage += errorMessage + "File[" + counter + "] - ERROR 404: No file selected" + "\n";
-        //                //    else if (returnedStatusCode == System.Net.HttpStatusCode.NoContent)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 204: File is empty" + "\n";
-        //                //    errorMessage += errorMessage + "File[" + counter + "] - ERROR 204: File is empty" + "\n";
-        //                //    else if (returnedStatusCode == System.Net.HttpStatusCode.NotAcceptable)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 406: File is missing header or footer or they are not prefixed with H or F" + "\n";
-        //                //    errorMessage += errorMessage + "File[" + counter + "] - ERROR 406: File is missing header or footer or they are not prefixed with H or F" + "\n";
-        //                //    else if (returnedStatusCode == System.Net.HttpStatusCode.BadRequest)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 400: Footer does not have the number of potential records" + "\n";
-        //                //    errorMessage += errorMessage + "File[" + counter + "] - ERROR 400: Footer does not have the number of potential records" + "\n";
-        //                //    else if (returnedStatusCode == System.Net.HttpStatusCode.PreconditionFailed)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 412: No record added to database because the number of footer records does not match with the number of existing records in the file" + "\n";
-        //                //    errorMessage += errorMessage + "File[" + counter + "] - ERROR 412: No record added to database because the number of footer records does not match with the number of existing records in the file" + "\n";
-        //                //    else if (returnedStatusCode == System.Net.HttpStatusCode.Conflict)
-        //                //        //ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 409: File with that header already exists in database" + "\n";
-        //                //    errorMessage += errorMessage + "File[" + counter + "] - ERROR 409: File with that header already exists in database" + "\n";
-        //                //    //else ViewBag.ErrorMessages = ViewBag.ErrorMessages + "File[" + counter + "] - ERROR 500: Internal Server Error" + "\n";
-        //                //    else errorMessage += errorMessage + "File[" + counter + "] - ERROR 500: Internal Server Error" + "\n";
-        //                //    counter++;
-
-        //                //    return Content(errorMessage);
-        //                //}
-        //            }
-
-        //        }
-        //        return Content("valjda");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return Content(ex.Message);
-        //    }
-        //}
         public ActionResult FlightRecordForm(Flight_Records sfr)
         {
             if (!String.IsNullOrEmpty(sfr.firstName) || !String.IsNullOrEmpty(sfr.lastName))
@@ -229,7 +147,15 @@ namespace AerLingus.Controllers
             return View();
         }
 
+        public ActionResult SearchFlightRecords()
+        {
+            return View();
+        }
 
+        [System.Web.Http.HttpGet]
+        public ActionResult GetSearchedFlightRecords(SearchFlightRecord search)
+        {
+
+        }
     }
-
 }
