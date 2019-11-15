d623e30c980b0bfc78003c18b5446e7c8089ab1a
Marko1410 <marko12917@its.edu.rs>
2019-11-15 17:39:12 +0100

    dodato Create

--- AerLingus/Controllers/Api/JourneySegmentsApiController.cs
+++ AerLingus/Controllers/Api/JourneySegmentsApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using AerLingus.Models;
 
@@ -25,5 +26,37 @@ namespace AerLingus.Controllers.Api
 
             return entities.JourneySegments.Where(js => js.TicketNo == ticketNo).AsEnumerable();
         }
+
+
+        [HttpPost]
+        [Route("api/JourneySegmentsApi/AddJourneySegment")]
+        public async Task<HttpResponseMessage> AddJourneySegmentAsync([FromBody] JourneySegment j)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (entities.JourneySegments.Any(b => b.TicketNo == j.TicketNo && b.couponNo ==j.couponNo))
+                        return Request.CreateResponse(HttpStatusCode.Conflict);
+                    else
+                    {
+                        entities.JourneySegments.Add(j);
+                        await entities.SaveChangesAsync();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+
+                }
+                else return Request.CreateResponse(HttpStatusCode.Conflict);
+            }
+
+
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+
     }
 }
--- AerLingus/Controllers/JourneySegmentsController.cs
+++ AerLingus/Controllers/JourneySegmentsController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AerLingus.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,47 @@ namespace AerLingus.Controllers
 {
     public class JourneySegmentsController : Controller
     {
+        private HttpClient client;
+        private HttpClient client1;
+        private List<JourneySegment> journeySegments;
+        private List<JourneySegment> listaSearch;
+        private AerLingus_databaseEntities entities;
+
+        public JourneySegmentsController()
+        {
+            client = new HttpClient();
+            client1 = new HttpClient();
+            journeySegments = new List<JourneySegment>();
+            listaSearch = new List<JourneySegment>();
+            entities = new AerLingus_databaseEntities();
+        }
+
         // GET: JourneySegments
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult AddJourneySegment (JourneySegment j)
+        {
+            client.BaseAddress = new Uri(@"http://localhost:54789/api/JourneySegmentsApi/AddJourneySegment");
+            var insertRecord = client.PostAsJsonAsync<JourneySegment>("", j);
+            insertRecord.Wait();
+            var recorddisplay = insertRecord.Result;
+            if (recorddisplay.IsSuccessStatusCode)
+            {
+                //redirekcija u listu svih flight rekorda
+                return View("UploadSuccessful");
+            }
+            else
+            {
+                return View("JournezSegmentForm");
+            }
+        }
+        public ActionResult JourneySegmentForm()
+        {
+            return View();
+        }
+
     }
 }
