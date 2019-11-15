252a0eafd4ccbe8750c5038bd8f2787ff7ea4fcd
Marko1410 <marko12917@its.edu.rs>
2019-11-15 17:41:34 +0100

    promena

--- AerLingus/Controllers/JourneySegmentsController.cs
+++ AerLingus/Controllers/JourneySegmentsController.cs
@@ -44,7 +44,7 @@ namespace AerLingus.Controllers
             }
             else
             {
-                return View("JournezSegmentForm");
+                return View("JourneySegmentForm");
             }
         }
         public ActionResult JourneySegmentForm()
